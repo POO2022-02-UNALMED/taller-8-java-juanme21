121dc7a6a0058924fc44d513427a21d70c5a8549
juanme21 <106550509+juanme21@users.noreply.github.com>
2022-10-16 11:39:55 -0500

    commit 1

---
+++ src/test/java/futbol/Futbolista.java
@@ -0,0 +1,60 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Futbolista> {
+
+	private String nombre;
+	private int edad;
+	private	final String posicion;
+	
+	
+	
+	public Futbolista(String nombre, int edad, String posicion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+
+	public Futbolista() {
+		this("Maradona", 30, "delantero");
+	}
+
+	public boolean equals(Futbolista f) {
+		return this.equals((Object) f);
+	}
+	
+	public abstract boolean jugarConLasManos();
+	
+	@Override
+	public String toString() {
+		return "El futbolista" + nombre + " tiene " + edad + ", y\r\n"
+				+ " juega de "+ posicion;
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		return 0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	
+}
---
+++ src/test/java/futbol/Jugador.java
@@ -0,0 +1,61 @@
+package futbol;
+
+import java.lang.Math;
+import java.util.Objects;
+
+public class Jugador extends Futbolista {
+
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+
+	public Jugador() {
+		super();
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(dorsal, golesMarcados);
+	}
+
+	@Override
+	public String toString() {
+		return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+ ", y juega de "+this.getPosicion()+" con el dorsal "+dorsal
+				+ ". Ha marcado "+ golesMarcados;
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		return Math.abs(this.getEdad()-o.getEdad());
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		return false;
+	}
+
+	public short getGolesMarcados() {
+		return golesMarcados;
+	}
+
+	public void setGolesMarcados(short golesMarcados) {
+		this.golesMarcados = golesMarcados;
+	}
+
+	public byte getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+
+}
